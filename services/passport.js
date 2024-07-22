845e6b29c96abf25402173f217972934864d843d
Rohan Rawat <rahonrawat194@gmail.com>
2024-07-22 09:46:44 +0530

    Google oAuth

---
+++ services/passport.js
@@ -0,0 +1,36 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const keys = require('../config/keys');
+
+const User = mongoose.model('user');
+
+passport.serializeUser((user,done) =>{
+    done(null,user.id);
+});
+
+passport.deserializeUser((id,done) =>{
+    User.findById(id).then(user => {
+        done(null,user.id);
+    });
+});
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSceret,
+    callbackURL: '/auth/google/callback',
+    proxy: true
+    }, 
+    (accessToken, refreshToken, profile, done) =>{
+        User.findOne({googleId: profile.id}).then(existingUser =>{
+            if(existingUser){
+                done(null,existingUser);
+            }
+            else{
+                new User({googleId: profile.id})
+                .save()
+                .then(user => done(null,user));
+            }
+        });
+    })
+);
